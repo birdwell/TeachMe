9ad2701a802d1f0963b2b82f2de5771ea5346773
Josh Birdwell <jbirdwell34@gmail.com>
2015-09-13 06:11:19 -0400

    delete

--- collections/collection.js
+++ collections/collection.js
@@ -14,16 +14,25 @@ angular.module('myApp.collection', ['ngRoute'])
         //Get ID out of current URL
         var ref = new Firebase('https://shining-fire-6589.firebaseio.com/collections/' + $routeParams.collectionId);
 		$firebaseObject(ref).$bindTo($scope,"collection");
-		
+
 		var auth = $firebaseAuth(ref);
 		auth.$onAuth(function(authData){
 			$scope.authData = authData;
 		});
-		
+
 		$scope.newresource = function(){
 			if($scope.collection.resources==null)
 				$scope.collection.resources=[];
 			$scope.collection.resources.push({});
 		}
+
+        $scope.deleteResource = function(resource,collection){
+            for (var i = 0; i < collection.resources.length; i++) {
+                if(collection.resources[i].$$hashKey == resource.$$hashKey){
+                    collection.resources.splice(i,1);
+                }
+            }
+
+        }
     }
 ]);
