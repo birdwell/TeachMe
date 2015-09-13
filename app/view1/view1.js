b41eaf26d183a3e20d39018811a73f3ccab9f2b4
Clive Chan <cc@clive.io>
2015-09-12 20:46:57 -0400

    more fix attempts

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,9 +9,10 @@ angular.module('myApp.view1', ['ngRoute','firebase','ui.bootstrap.modal','ngTags
   });
 }])
 
-.controller('View1Ctrl', ['$scope','$firebaseArray','$http','$firebaseAuth','$firebaseObject', function($scope, $firebaseArray, $http, $firebaseAuth,$firebaseObject) {
+.controller('View1Ctrl', ['$scope','$firebaseAuth','$firebaseObject', function($scope, $firebaseAuth,$firebaseObject) {
     var ref = new Firebase('https://shining-fire-6589.firebaseio.com');
     var auth = $firebaseAuth(ref);
+	
     $scope.showModal = false;
     $scope.authData = null;
 	
@@ -29,12 +30,9 @@ angular.module('myApp.view1', ['ngRoute','firebase','ui.bootstrap.modal','ngTags
 		   { text: 'Tag3' }
 		   ];
 	
-    $scope.loadTags = function(query) {
-	    return $http.get('tags.json');
-	};
-	
 	$scope.processKeypress = function($event){
-		if($event.which==13)$scope.addCollection();
+		if($event.which==13)
+			$scope.addCollection();
 	}
 
     $scope.addCollection = function() {
@@ -46,7 +44,7 @@ angular.module('myApp.view1', ['ngRoute','firebase','ui.bootstrap.modal','ngTags
           console.log("Authenticated user with uid:", $scope.authData.uid);
         }
 
-		var ind = Math.floor(Math.random()*1000000)+Math.floor(Math.random()*1000000)+Math.floor(Math.random()*1000000);
+		var ind = generatePushID();
 		$scope.collections[ind] = {
 			title: title,
 			type: type,
@@ -56,8 +54,68 @@ angular.module('myApp.view1', ['ngRoute','firebase','ui.bootstrap.modal','ngTags
 			date: Date.now()
 		};
 		
-		$scope.users[$scope.authData.uid].collectionIDs.push(ind);
+		$scope.users[$scope.authData.uid].collectionIDs[generatePushID()]=ind;
     };
+	
+	//https://gist.github.com/mikelehen/3596a30bd69384624c11
+	/**
+	 * Fancy ID generator that creates 20-character string identifiers with the following properties:
+	 *
+	 * 1. They're based on timestamp so that they sort *after* any existing ids.
+	 * 2. They contain 72-bits of random data after the timestamp so that IDs won't collide with other clients' IDs.
+	 * 3. They sort *lexicographically* (so the timestamp is converted to characters that will sort properly).
+	 * 4. They're monotonically increasing.  Even if you generate more than one in the same timestamp, the
+	 *    latter ones will sort after the former ones.  We do this by using the previous random bits
+	 *    but "incrementing" them by 1 (only in the case of a timestamp collision).
+	 */
+	var generatePushID = (function() {
+	  // Modeled after base64 web-safe chars, but ordered by ASCII.
+	  var PUSH_CHARS = '-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz'.split("").reverse().join("");
+
+	  // Timestamp of last push, used to prevent local collisions if you push twice in one ms.
+	  var lastPushTime = 0;
+
+	  // We generate 72-bits of randomness which get turned into 12 characters and appended to the
+	  // timestamp to prevent collisions with other clients.  We store the last characters we
+	  // generated because in the event of a collision, we'll use those same characters except
+	  // "incremented" by one.
+	  var lastRandChars = [];
+
+	  return function() {
+		var now = new Date().getTime();
+		var duplicateTime = (now === lastPushTime);
+		lastPushTime = now;
+
+		var timeStampChars = new Array(8);
+		for (var i = 7; i >= 0; i--) {
+		  timeStampChars[i] = PUSH_CHARS.charAt(now % 64);
+		  // NOTE: Can't use << here because javascript will convert to int and lose the upper bits.
+		  now = Math.floor(now / 64);
+		}
+		if (now !== 0) throw new Error('We should have converted the entire timestamp.');
+
+		var id = timeStampChars.join('');
+
+		if (!duplicateTime) {
+		  for (i = 0; i < 12; i++) {
+			lastRandChars[i] = Math.floor(Math.random() * 64);
+		  }
+		} else {
+		  // If the timestamp hasn't changed since last push, use the same random number, except incremented by 1.
+		  for (i = 11; i >= 0 && lastRandChars[i] === 63; i--) {
+			lastRandChars[i] = 0;
+		  }
+		  lastRandChars[i]++;
+		}
+		for (i = 0; i < 12; i++) {
+		  id += PUSH_CHARS.charAt(lastRandChars[i]);
+		}
+		if(id.length != 20) throw new Error('Length should be 20.');
+
+		return id;
+	  };
+	})();
+	//end generatePushID()
 	
 	$scope.upvote = function(collection){
 		if(!$scope.authData)alert("Log in to do that");
@@ -79,21 +137,8 @@ angular.module('myApp.view1', ['ngRoute','firebase','ui.bootstrap.modal','ngTags
 	$scope.upvoted = function(collection){
 		return $scope.authData && collection.upvotes && collection.upvotes[$scope.authData.uid];
 	};
-	
-    $scope.open = function() {
-      $scope.showModal = true;
-      $scope.collection = {type:"References"};
-    };
-
-    $scope.ok = function() {
-      $scope.showModal = false;
-    };
-
-    $scope.cancel = function() {
-      $scope.showModal = false;
-    };
-
-    $scope.submit = function() {
-      $scope.showModal = false;
-    }
+	$scope.resetCollectionForm = function(){
+		$scope.newcollection = {"type":"Reference"};
+	};
+	$scope.resetCollectionForm();
 }])
